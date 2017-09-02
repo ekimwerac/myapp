cff506f82b8ec31d298673e915df4caa103b92b7
mike carew <mike@carew.uk>
2017-09-01 21:54:24 -0400

    Added Lab1

---
+++ myapp/src/main/java/com/carewassoc/Main.java
@@ -0,0 +1,12 @@
+package com.carewassoc;
+
+public class Main {
+
+	public Person person1;
+	public Book book1;
+	
+	person1 = new Person("Mikey", "Carewski");
+	book1 = new book("T10", "Mikeys book", "Mikey carews book", 25.00);
+	
+	
+}
